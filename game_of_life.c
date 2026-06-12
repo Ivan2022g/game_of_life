533cb35e9226fd15851d45f25f9137cbffa4f5dd
thrynnza <thrynnza@student.21-school.ru>
2026-06-12 13:48:38 +0700

    refactoring

--- game_of_life.c
+++ game_of_life.c
@@ -2,36 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SPASE ' '
+#include "settings.h"
+#include "structures.h"
 #define CANT_START_GAME "Не удалось инициализировать игру"
-#define WIDTH 80
-#define HEIGHT 25
 
-struct GameState {
-    // Текущее состояние поля (1 - живая, 0 - мёртвая)
-    unsigned char current[HEIGHT][WIDTH];
-
-    // Буфер для следующего поколения (для одновременного обновления)
-    unsigned char next[HEIGHT][WIDTH];
-
-    // Скорость смены поколений (задержка в миллисекундах)
-    int delay_ms;
-
-    // Флаги состояния игры
-    int is_running;  // 1 - игра активна, 0 - завершена
-    int is_stable;   // 1 - достигнута стабильная конфигурация
-    int population;  // Текущее количество живых клеток
-
-    // Статистика для определения стабильности
-    unsigned long generation;              // Номер текущего поколения
-    unsigned long last_population_change;  // Когда последний раз менялась популяция
-};
-
-// Вспомогательная структура для координат
-struct Cell {
-    int x;  // от 0 до WIDTH-1
-    int y;  // от 0 до HEIGHT-1
-};
 void process(const struct GameState game_state);
 int init(int argc, char* argv[], struct GameState game_state);
 void wait_for_action(int action);
---
+++ settings.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define SPASE ' '
+#define WIDTH 80
+#define HEIGHT 25
+#define SPEED_UP_1 A
+#define SPEED_UP_1 a
+#define SPEED_DOWN_1 Z
+#define SPEED_DOWN_2 z
